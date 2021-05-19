db4aeacbef534690732ec3a0ea176d06117e3156
zhangkang <zhangkang@lvmama.com>
2021-05-19 15:26:26 +0800

    ssi大功告成

--- simple-demo/src/main/java/org/zk/simpledemo/controller/AnnotationController.java
+++ simple-demo/src/main/java/org/zk/simpledemo/controller/AnnotationController.java
@@ -2,19 +2,27 @@ package org.zk.simpledemo.controller;
 
 import org.zk.simple.spring.web.bind.annotation.RequestMapping;
 import org.zk.simple.spring.web.servlet.ModelAndView;
+import org.zk.simpledemo.domain.User;
+import org.zk.simpledemo.service.UserService;
+import org.zk.simplespring.beans.factory.annotation.Autowired;
 import org.zk.simplespring.stereotype.Controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
 public class AnnotationController {
 
-	@RequestMapping("/test")
-	public ModelAndView test() {
+	@Autowired
+	private UserService userService;
+
+	@RequestMapping("/list")
+	public ModelAndView list() {
+		List<User> userList = userService.findAll();
 		Map<String, Object> model = new HashMap<>();
-		model.put("username", "zk");
+		model.put("username", userList.get(0).getUsername());
 		return new ModelAndView("hello", model);
 	}
 }
